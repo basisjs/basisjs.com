f97b8c941201a2f3942fdd4d96135cf8d343a07f
lahmatiy <rdvornov@gmail.com>
2012-08-23 17:01:51 +0600

    change host

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ var bt = require('basis-devtools');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Heroku!\n');
-}).listen(process.env.PORT || 8080, process.env.HOST || '127.0.0.1');
+}).listen(process.env.PORT || 8080, process.env.HOST || '0.0.0.0');
 
-console.log('Server running at http://' + [process.env.PORT || 8080, process.env.HOST || '127.0.0.1'].reverse().join(':') + '/');
+console.log('Server running at http://' + [process.env.PORT || 8080, process.env.HOST || '0.0.0.0'].reverse().join(':') + '/');
