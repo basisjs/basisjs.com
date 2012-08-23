d130b11ba56a252027a3f22661f7e04e19c9c131
lahmatiy <rdvornov@gmail.com>
2012-08-23 17:09:07 +0600

    use basis server

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var http = require('http');
 var bt = require('basis-devtools');
 
-//bt.server.launch({ base: process.cwd(), ignore: ['.git', 'basis/.git'] });
+bt.server.launch({
+  port: process.env.PORT || 8000,
+  base: process.cwd(),
+  ignore: ['.git', 'basis/.git']
+});
 
-http.createServer(function (req, res) {
+/*http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Heroku!\n');
 }).listen(process.env.PORT || 8080, process.env.HOST || '0.0.0.0');
 
-console.log('Server running at http://' + [process.env.PORT || 8080, process.env.HOST || '0.0.0.0'].reverse().join(':') + '/');
+console.log('Server running at http://' + [process.env.PORT || 8080, process.env.HOST || '0.0.0.0'].reverse().join(':') + '/');*/
