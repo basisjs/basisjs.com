15a7a8b565922e5b33af50880d2f3af9d952b8d4
lahmatiy <rdvornov@gmail.com>
2012-08-24 20:28:18 +0600

    server launch changed

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 var http = require('http');
 var bt = require('basis-devtools');
 
-bt.server.launch({
+bt.server.command(null, {
   port: process.env.PORT || 8000,
-  base: process.cwd(),
   ignore: ['.git', 'basis/.git']
 });
-
-/*http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Heroku!\n');
-}).listen(process.env.PORT || 8080, process.env.HOST || '0.0.0.0');
-
-console.log('Server running at http://' + [process.env.PORT || 8080, process.env.HOST || '0.0.0.0'].reverse().join(':') + '/');*/
+/*bt.server.launch({
+  port: process.env.PORT || 8000,
+  base: process.cwd(),
+  ignore: ['.git', 'basis/.git'],
+  sync1: false
+});
+*/
