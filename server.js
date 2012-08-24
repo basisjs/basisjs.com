534e09f99f6526d5ccb271602293993762e02673
lahmatiy <rdvornov@gmail.com>
2012-08-24 20:42:22 +0600

    move basisjs

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bt = require('basis-devtools');
 
 bt.server.command(null, {
   port: process.env.PORT || 8000,
-  ignore: ['.git', 'basis/.git'],
+  ignore: ['.git'],
   base: 'site'
 });
 /*bt.server.launch({
