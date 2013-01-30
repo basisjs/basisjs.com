c480990e18bfc27529f95fad2074d76bc5a7bed8
Roman Dvornov <rdvornov@gmail.com>
2013-01-30 06:22:09 +0400

    html handler, short paths

---
+++ html.js
@@ -0,0 +1,16 @@
+
+var fs = require('fs');
+var path = require('path');
+var script = fs.readFileSync('./google-analytics.script', 'utf-8');
+
+exports.process = function(mime, data, fn, fres){
+  if (mime == 'text/html')
+  {
+    data = String(data).replace(/<\/body>/i, script + '</body>');
+
+    if (fres.rewritten)
+      data = data.replace(/<head>/i, '<head><base href="' + path.dirname(path.normalize(fn)) + '/">');
+
+    return data;
+  }
+};
--- server.js
+++ server.js
@@ -4,5 +4,14 @@ require('basisjs-tools').server.command(null, {
   base: 'site',
   index: 'basisjs',
   sync: false,
-  dev: false
+  dev: false,
+  handler: [
+    'html.js'
+  ],
+  rewrite: {
+    '^/docs(.*)': '/basisjs/docs/$1',
+    '^/demo/(.*)': '/basisjs/demo/$1',
+    '^/src/(.*)': '/basisjs/src/$1',      
+    '^/test/(.*)': '/basisjs/test/$1'      
+  }
 });
